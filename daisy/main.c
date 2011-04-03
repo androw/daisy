c25396b5d83844bcf970858c4edfa51c7b632513
Nicolas LORIN <Androw@androwbook.home.androw.eu>
2011-04-03 19:25:00 +0200

    Initial Commit

---
+++ daisy/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  daisy
+//
+//  Created by Nicolas LORIN on 03/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
